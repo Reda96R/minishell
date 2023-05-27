dee465769f478c7197db411ceb67e3652feba27e
Reda96R <redarayyad@yahoo.com>
2023-05-27 19:55:47 +0100

    setting vars in a seprate function

--- src/minishell.c
+++ src/minishell.c
@@ -1,20 +1,33 @@
 #include "../includes/minishell.h"
 
-int main(int ac, char *av[], char *env[])
+int ft_env_setter(char **vars, char **env)
 {
-  int   i;
-  char  **vars;
+  int i;
 
   i = 0;
   while (env[i])
     i++;
-  if (!(vars = (char **)malloc(sizeof (char *) * i + 1)))
+  if (!(vars = (char **)malloc (sizeof (char *) * i + 1)))
     return (0);
   i = 0;
   while (env[i])
   {
     vars[i] = ft_strdup((const char *)env[i]);
+    if (!vars[i])
+    {
+      free (vars);
+      return (0);
+    }
     i++;
   }
-    vars[i] = 0;
+  vars[i] = 0;
+  return (1);
+}
+
+int main(int ac, char *av[], char *env[])
+{
+  char  **vars;
+
+  if (!ft_env_setter(vars, env))
+    return (0);
 }
