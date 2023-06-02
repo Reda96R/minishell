3adcdd0a48d6a3db889cd8be6ebddb39ee516635
Yns-Sl <younes.slimani70@gmail.com>
2023-06-02 14:50:13 +0100

    push

---
+++ execution/includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 20:26:27 by yes-slim          #+#    #+#             */
+/*   Updated: 2023/06/02 14:50:02 by yes-slim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <string.h>
+
+
+#endif
