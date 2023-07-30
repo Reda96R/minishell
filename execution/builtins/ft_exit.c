2379305ce777c0e87addb7963404af8ba4e4959b
Younes Es-Slimani <yes-slim@e1r6p3.1337.ma>
2023-07-30 13:11:00 +0100

    norm error for cd

--- execution/builtins/ft_exit.c
+++ execution/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:40:40 by yes-slim          #+#    #+#             */
-/*   Updated: 2023/07/29 17:10:53 by yes-slim         ###   ########.fr       */
+/*   Updated: 2023/07/30 13:10:56 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ int		check_numeriq(char *str)
 void	ft_exit(t_cmds *cmd)
 {
 	int				i;
-	unsigned char	exit_code;
 	
 	i = 1;
 	if (!cmd->str[1])
@@ -51,6 +50,8 @@ void	ft_exit(t_cmds *cmd)
 		ft_builtins_error(6, NULL);
 		return ;
 	}
-	exit_code = _atoi(cmd->str[1]);
+	
+	
+	g_var.exit_status = _atoi(cmd->str[1]);
 	exit(g_var.exit_status);
 }
