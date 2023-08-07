e66527ee9bb80c059267ebcf9109330337cd9ab5
yes-slim <younes.slimani70@gmail.com>
2023-08-07 20:22:48 +0100

    working on heredoc : still expand only

--- execution/helpers/_heredoc.c
+++ execution/helpers/_heredoc.c
@@ -6,27 +6,46 @@
 /*   By: YOUNES <YOUNES@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 15:32:30 by YOUNES            #+#    #+#             */
-/*   Updated: 2023/08/07 18:02:29 by YOUNES           ###   ########.fr       */
+/*   Updated: 2023/08/07 20:21:55 by YOUNES           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	ft_wait_hd(int pid)
+{
+	int	status;
+
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == 3)
+			g_var.exit_status = 131;
+		if (WTERMSIG(status) == 2)
+			g_var.exit_status = 130;
+	}
+}
 int	ft_heredoc(t_mylxr *del)
 {
 	int	pp[2];
 	char *str;
 
 	pipe(pp);
-	while (1)
-	{
-		signal(SIGQUIT, SIG_IGN);
-		str = readline("> ");
-		if (!str || !_strcmp(del->str, str))
-			break ;
-		_dprintf(pp[1], "%s\n", str);
-		free(str);
+	pid_t pid = fork();
+	if (pid == 0)
+	{	
+		signal(SIGQUIT, SIG_DFL);
+		signal(SIGINT, SIG_DFL);
+		while (1)
+		{
+			str = readline("> ");
+			if (!str || !_strcmp(del->str, str))
+				exit(1);
+			_dprintf(pp[1], "%s\n", str);
+			free(str);
+		}
 	}
-	signals();
+	ft_wait_hd(pid);
+	close(pp[1]);
 	return (pp[0]);
 }
