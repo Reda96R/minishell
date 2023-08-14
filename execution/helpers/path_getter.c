874e4ae1dc09e63966d87208551f2cec7399eea7
Younes Es-Slimani <yes-slim@c4r8p1.1337.ma>
2023-08-14 19:59:51 +0100

    fixing heredoc/fd_in/fd_out

--- execution/helpers/path_getter.c
+++ execution/helpers/path_getter.c
@@ -6,7 +6,7 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:27:00 by yes-slim          #+#    #+#             */
-/*   Updated: 2023/08/13 04:35:38 by yes-slim         ###   ########.fr       */
+/*   Updated: 2023/08/14 19:31:17 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*get_path(char *str, char **path)
 	i = 0;
 	while (path[i])
 	{
-		cmd = ft_strjoin(path[i], str);
+		cmd = _strjoin(path[i], str);
 		if (access(cmd, X_OK) == -1)
 			i++;
 		else
@@ -40,14 +40,14 @@ char	*path_getter(t_cmds *init)
 		if (access(init->str[0], X_OK) == -1)
 			ft_error_exec(1, init->str[0], -1);
 		else
-			return (strdup(init->str[0]));
+			return (_strdup(init->str[0]));
 	}
 	if (init->str[0][0] == '.' && init->str[0][1] == '/')
 	{
 		if (access(init->str[0], X_OK) == -1)
 			ft_error_exec(1, init->str[0], -1);
 		else
-			return (strdup(init->str[0]));
+			return (_strdup(init->str[0]));
 	}
 	path = get_path(init->str[0], g_var.data->paths);
 	if (!path)
