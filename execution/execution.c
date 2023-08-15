233525a4f8ce6daa4e729879a4a43fd6dbced3e9
Younes Es-Slimani <yes-slim@c4r1p3.1337.ma>
2023-08-15 17:15:15 +0100

    fixing cmds

--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 18:57:34 by yes-slim          #+#    #+#             */
-/*   Updated: 2023/08/15 15:05:27 by yes-slim         ###   ########.fr       */
+/*   Updated: 2023/08/15 17:00:58 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void	ft_execution(t_data *init)
 	if (init->cmds)
 	{
 		_heredoc(init);
-		if (!init->pipes)
+		if (!init->cmds->next)
 			one_cmd(init->cmds);
 		else
 			multiple_cmds(init);
